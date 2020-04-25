60ca42404948709cb892466c10bc7084782b5cee
scfox <steve@foxgroupco.com>
2020-04-24 19:08:14 -0500

    Fix input path.

--- src/tune_model.py
+++ src/tune_model.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     start = datetime.now()
     print(f"Starting hyper parameter optimizations at: {start}")
     _clear_logs()
-    x_train, x_test, y_train, y_test = _load_data(model_dir+'input')
+    x_train, x_test, y_train, y_test = _load_data('model/input')
     x_train, y_train = training_xform(x_train, y_train)
 
     tf.config.threading.set_inter_op_parallelism_threads = 0
