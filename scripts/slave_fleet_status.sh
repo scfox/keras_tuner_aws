4c0da0e4b842403decfc4121f0dc24e48fdfd6a8
scfox <steve@foxgroupco.com>
2020-04-25 04:59:21 -0500

    Remove retry loop on slave for hardening testing.

--- scripts/slave_fleet_status.sh
+++ scripts/slave_fleet_status.sh
@@ -1,1 +1,2 @@
 aws ec2 describe-fleets --filters '{"Name": "activity-status", "Values": ["fulfilled"]}' --profile agilemobile
+aws ec2 describe-fleets --filters '{"Name": "fleet-state", "Values": ["active"]}' --profile agilemobile
--- tune_slave.sh
+++ tune_slave.sh
@@ -3,8 +3,4 @@ export KERASTUNER_TUNER_ID="tuner0"
 export KERASTUNER_ORACLE_IP="$1"
 export KERASTUNER_ORACLE_PORT="8000"
 sleep 10
-while :
-do
-  python src/tune_model.py
-  echo "restarting..."
-done
+python src/tune_model.py
