da4c7c5c50eea6ef675ca8d9ae9658b15549ecc3
scfox <steve@foxgroupco.com>
2020-04-25 06:43:47 -0500

    Set TUNER_ID to private IP to distinguish slaves.

--- tune_slave.sh
+++ tune_slave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/anaconda3/bin/activate tensorflow2_p36
-export KERASTUNER_TUNER_ID="tuner0"
+export KERASTUNER_TUNER_ID=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 export KERASTUNER_ORACLE_IP="$1"
 export KERASTUNER_ORACLE_PORT="8000"
 cd keras_tuner_aws
