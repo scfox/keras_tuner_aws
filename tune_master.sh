c639e801447207f12000c28eb8f4ec4753cc1b1b
scfox <steve@foxgroupco.com>
2020-04-25 09:08:10 -0500

    Master gets parameters passed to it as well so tensorboard points at correct path.

--- tune_master.sh
+++ tune_master.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
+source /home/ec2-user/anaconda3/bin/activate tensorflow2_p36
 export KERASTUNER_TUNER_ID="chief"
 export KERASTUNER_ORACLE_IP="0.0.0.0"
 export KERASTUNER_ORACLE_PORT="8000"
-while :
-do
-  python src/tune_model.py
-  echo "restarting..."
-done
+tensorboard --logdir=${2}/logs --port 6012 --bind_all &
+python src/tune_model.py --output_path=$2 --max_trials=$3 --max_epochs=$4
--- tune_slave.sh
+++ tune_slave.sh
@@ -3,6 +3,9 @@ source /home/ec2-user/anaconda3/bin/activate tensorflow2_p36
 export KERASTUNER_TUNER_ID=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 export KERASTUNER_ORACLE_IP="$1"
 export KERASTUNER_ORACLE_PORT="8000"
-cd keras_tuner_aws
 pip install -r src/requirements.txt
-python src/tune_model.py --output_path=$2 --max_trials=$3 --max_epochs=$4
+while :
+do
+  python src/tune_model.py --output_path=$2 --max_trials=$3 --max_epochs=$4
+  echo "restarting..."
+done
